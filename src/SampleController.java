9bbbea1dce8da368bb3071b45363780499e0c90e
Ahmed El Hechmi Jhider <jhiderahmed0@gmail.com>
2024-04-07 01:37:23 +0100

    login/signup page ui

--- src/SampleController.java
+++ src/SampleController.java
@@ -1,12 +1,21 @@
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+import javax.swing.*;
 
 
 public class SampleController {
 
     @FXML
     private ListView<String> listView;
+    @FXML
+    private AnchorPane welcome_page;
+    @FXML
+    private AnchorPane signup_page;
+    @FXML
+    private AnchorPane login_page;
 
     public void addmessage() {
         listView.getItems().add("Ahmed: i clicked the button !");
@@ -14,5 +23,19 @@ public class SampleController {
 
     public void sendClick(ActionEvent event) {
         addmessage();
+    }
+    public void signup(ActionEvent event){
+        welcome_page.setVisible(false);
+        signup_page.setVisible(true);
+    }
+    public void login(ActionEvent event){
+        welcome_page.setVisible(false);
+        login_page.setVisible(true);
+    }
+    public void esc(ActionEvent event){
+        welcome_page.setVisible(true);
+        signup_page.setVisible(false);
+        login_page.setVisible(false);
+
     }
 }
